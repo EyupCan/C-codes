de00daffd53beb53ec30e9e4aa6d9fee8e66b3d9
EyupCanKılınçdemir <eyupcankilincdemir@gmail.com>
2015-07-06 04:13:25 +0300

    girilen karakterin tam sayi degerini basar

---
+++ 8-karakter_tam_sayi.c
@@ -0,0 +1,22 @@
+/********************************************
+*                                           *
+*           Eyüp Can KILINÇDEMİR            *
+*      KARADENİZ TEKNİK UNİVERSİTESİ        *
+*          ceksoft.wordpress.com            *
+*       eyupcankilincdemir@gmail.com        *
+*                                           *
+********************************************/
+
+#include<stdio.h>
+
+int main()
+{
+    char karakter;
+
+    printf("Bir karakter giriniz:");
+    scanf("%c",&karakter);
+
+    printf("Karakterin tam sayi degeri %d dir.",karakter);
+    return 0;
+}
+
