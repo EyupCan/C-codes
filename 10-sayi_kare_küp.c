ae818c645bd5ec9fe59827ab84cbfaac145caa0b
EyupCanKılınçdemir <eyupcankilincdemir@gmail.com>
2015-07-06 04:34:40 +0300

    10tane sayinin karesi küpü

---
+++ 10-sayi_kare_küp.c
@@ -0,0 +1,29 @@
+/********************************************
+*                                           *
+*           Eyüp Can KILINÇDEMİR            *
+*      KARADENİZ TEKNİK UNİVERSİTESİ        *
+*          ceksoft.wordpress.com            *
+*       eyupcankilincdemir@gmail.com        *
+*                                           *
+********************************************/
+
+#include<stdio.h>
+
+int main()
+{
+    printf("Sayi\tKaresi\tKupu\t\n");
+    printf("%d\t%d\t%d\n",0,0*0,0*0*0);
+    printf("%d\t%d\t%d\n",1,1*1,1*1*1);
+    printf("%d\t%d\t%d\n",2,2*2,2*2*2);
+    printf("%d\t%d\t%d\n",3,3*3,3*3*3);
+    printf("%d\t%d\t%d\n",4,4*4,4*4*4);
+    printf("%d\t%d\t%d\n",5,5*5,5*5*5);
+    printf("%d\t%d\t%d\n",6,6*6,6*6*6);
+    printf("%d\t%d\t%d\n",7,7*7,7*7*7);
+    printf("%d\t%d\t%d\n",8,8*8,8*8*8);
+    printf("%d\t%d\t%d\n",9,9*9,9*9*9);
+    printf("%d\t%d\t%d\n",10,10*10,10*10*10);
+
+    return 0;
+}
+
